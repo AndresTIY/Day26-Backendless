974cd1e55a52d23527e19232d36c38b4870e9ffe
Andres Cifuentes <aacifuentes@gmail.com>
2017-04-05 16:45:51 -0500

    styled task items and added state buttons

--- app/scripts/task-view.js
+++ app/scripts/task-view.js
@@ -2,37 +2,59 @@
 export default function(store){
 
   let state = store.getState();
-  console.log(state.tasks);
 
-  state.tasks.forEach(function(item, i, arr){
-    console.log(item.task);
-  })
+
+
 
 
   let $htmlTasks = $(`
     <div class="task-card">
       <h2>My To Do List</h2>
       <ul class="list">
-
       </ul>
-      <label for="add-task-btn">Add more to your list!</label>
-      <input placeholder="new task" class="add-task" type="text" name="" value="">
-      <button class="add-item" type="button" name="button">Add Task</button>
-      <input placeholder="task description" class="add-desc" type="text" name="" value="">
-      <button class="add-desc-btn" type="button" name="button">Add Description</button>
+      <div class="form">
+        <h3>Add More Tasks</h3>
+        <input placeholder="new task" class="add-task" type="text" name="" value="">
+        <input placeholder="task description" class="add-desc" type="text" name="" value=""><br>
+        <label for="due-date">Due Date</label>
+        <input class="due-date" type="datetime-local" name="" value="">
+        <label for="important">Make this a priority?</label>
+        <input id="important" type="checkbox" name="" value="">
+        <button class="add-item" type="button" name="button">Add Task</button>
+      </div>
+
     </div>`)
 
 
-  let $htmlTaskItem = $(`
-    <li>
-      <button class="del-btn">X</button>
-      <span class="task-name">task name</span><br>
-      <span class="description">how long can this description be</span>
 
-    </li>
-`)
+    function taskItemView(item){
+      let $htmlTaskItem = $(`
+        <li>
 
-  $($htmlTasks).find('.list').html($htmlTaskItem)
+
+
+          <button class="del-btn">X</button>
+          <span class="task-name">${item.task}</span>
+          <span class="exclamation"> ! </span><br>
+          <span class="description">${item.description}</span><br>
+          <button class="started">Started?</button>
+
+          <button class="completed">Completed?</button>
+        </li>`)
+        //code for button
+
+      return $htmlTaskItem;
+    }
+
+    var taskItems = state.tasks.map(function(item, i, arr){
+      return taskItemView(item);
+    });
+
+
+
+
+
+  $($htmlTasks).find('.list').html(taskItems)
 
   return $htmlTasks
 }
